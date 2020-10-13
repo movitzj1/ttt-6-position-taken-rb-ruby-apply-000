1aa03d6641f8e86c5f8a53a3eeec2e044c0daf49
Justin Movitz <movitzj1@montclair.edu>
2020-10-13 19:23:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
-    return false
+    return true
   end
   if board[index] == " " || "" || nil
-    return true
+    return false
   end
 end  
